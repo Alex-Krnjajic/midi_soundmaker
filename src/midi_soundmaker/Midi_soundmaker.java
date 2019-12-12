2d3d1f9f8cb9a10b9941a708f03cdb0876c3b1bf
Don-Cheadle <alexk753@gmail.com>
2019-12-12 10:12:36 +0100

    Initial commit

---
+++ src/midi_soundmaker/Midi_soundmaker.java
@@ -0,0 +1,14 @@
+package midi_soundmaker;
+
+public class Midi_soundmaker {
+
+	public Midi_soundmaker() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
