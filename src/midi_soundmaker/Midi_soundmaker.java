e465baf8318bc7a5cf5a16d9b819509acd9ed4ad
Don-Cheadle <alexk753@gmail.com>
2019-12-20 13:06:34 +0100

    ver 1.0

--- src/midi_soundmaker/Midi_soundmaker.java
+++ src/midi_soundmaker/Midi_soundmaker.java
@@ -1,108 +1,88 @@
 package midi_soundmaker;
 
-import javax.sound.midi.*; 
-import java.util.*; 
-import javax.swing.SwingUtilities;
+import javax.sound.midi.*;
+import java.util.*;
 
 public class Midi_soundmaker {
-	
-	static int numOfNotes = 0;
 
-	public Midi_soundmaker() {
-		
-			}
+	static int numOfNotes = 0;
+	static ArrayList<Integer> notes = new ArrayList<Integer>();
+	int counter = 0;
+	int timing = 0;
 
 	public static void main(String[] args) {
-		UI ui = new UI();
-		SwingUtilities.invokeLater(ui);
-		
-		System.out.println("Enter the number of notes to be played: "); 
-        Scanner in = new Scanner(System.in); 
-        numOfNotes = in.nextInt(); 
-  
-       
+
+		new UI("Key Listener Tester");
 
 	}
-	public void setUpPlayer(int numOfNotes) 
-    { 
-  
-        try { 
-  
-            // A static method of MidiSystem that returns 
-            // a sequencer instance. 
-            Sequencer sequencer = MidiSystem.getSequencer(); 
-            sequencer.open(); 
-  
-            // Creating a sequence. 
-            Sequence sequence = new Sequence(Sequence.PPQ, 4); 
-  
-            // PPQ(Pulse per ticks) is used to specify timing 
-            // type and 4 is the timing resolution. 
-  
-            // Creating a track on our sequence upon which 
-            // MIDI events would be placed 
-            Track track = sequence.createTrack(); 
-             
-  
-                // Adding some events to the track 
-                for (int i = 5; i < (4 * numOfNotes) + 5; i += 4) 
-            { 
-  
-                // Add Note On event 
-                track.add(makeEvent(144, 1, i, 100, i)); 
-  
-                // Add Note Off event 
-                track.add(makeEvent(128, 1, i, 100, i + 2)); 
-            } 
-  
-            // Setting our sequence so that the sequencer can 
-            // run it on synthesizer 
-            sequencer.setSequence(sequence); 
-  
-            // Specifies the beat rate in beats per minute. 
-            sequencer.setTempoInBPM(220); 
-  
-            // Sequencer starts to play notes 
-            sequencer.start(); 
-  
-            while (true) { 
-  
-                // Exit the program when sequencer has stopped playing. 
-                if (!sequencer.isRunning()) { 
-                    sequencer.close(); 
-                    break;
-                } 
-            } 
-        } 
-        catch (Exception ex) { 
-  
-            ex.printStackTrace(); 
-        } 
-    } 
-  
-    public MidiEvent makeEvent(int command, int channel, 
-                               int note, int velocity, int tick) 
-    { 
-  
-        MidiEvent event = null; 
-  
-        try { 
-  
-            // ShortMessage stores a note as command type, channel, 
-            // instrument it has to be played on and its speed. 
-            ShortMessage a = new ShortMessage(); 
-            a.setMessage(command, channel, note, velocity); 
-  
-            // A midi event is comprised of a short message(representing 
-            // a note) and the tick at which that note has to be played 
-            event = new MidiEvent(a, tick); 
-        } 
-        catch (Exception ex) { 
-  
-            ex.printStackTrace(); 
-        } 
-        return event; 
-    } 
+
+	public void setUpPlayer(int numOfNotes) {
+
+		try {
+
+			// A static method of MidiSystem that returns
+			// a sequencer instance.
+			Sequencer sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+
+			// Creating a sequence.
+			Sequence sequence = new Sequence(Sequence.PPQ, 4);
+
+			// PPQ(Pulse per ticks) is used to specify timing
+			// type and 4 is the timing resolution.
+
+			// Creating a track on our sequence upon which
+			// MIDI events would be placed
+			Track track = sequence.createTrack();
+			counter = 0;
+			timing = 1;
+			System.out.print(notes);
+			System.out.println(numOfNotes);
+			// Adding some events to the track
+			for (int i = 0; i < numOfNotes; i++) {
+
+				// Add Note On event
+				track.add(makeEvent(144, 1, notes.get(counter), 100, timing));
+				System.out.print("note on ");
+
+				// Add Note Off event
+				track.add(makeEvent(128, 1, notes.get(counter), 100, timing + 2));
+				System.out.print("note off");
+
+				counter++;
+				timing += 3;
+			}
+
+			// Setting our sequence so that the sequencer can
+			// run it on synthesizer
+			sequencer.setSequence(sequence);
+
+			// Specifies the beat rate in beats per minute.
+			sequencer.setTempoInBPM(220);
+
+			// Sequencer starts to play notes
+			sequencer.start();
+
+		} catch (Exception ex) {
+
+			ex.printStackTrace();
+		}
+	}
+
+	public MidiEvent makeEvent(int command, int channel, int note, int velocity, int tick) {
+
+		MidiEvent event = null;
+
+		try {
+
+			ShortMessage a = new ShortMessage();
+			a.setMessage(command, channel, note, velocity);
+
+			event = new MidiEvent(a, tick);
+		} catch (Exception ex) {
+
+			ex.printStackTrace();
+		}
+		return event;
+	}
 }
-
-
--- src/midi_soundmaker/UI.java
+++ src/midi_soundmaker/UI.java
@@ -1,60 +1,112 @@
 package midi_soundmaker;
 
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import javax.swing.WindowConstants;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class UI implements Runnable {
-	
-	static String numOfNotes;
+@SuppressWarnings("serial")
+public class UI extends JFrame implements KeyListener {
 
-	private JFrame frame;
-	static JButton addButton = new JButton("+");
-	static JButton playButton = new JButton("play");
+	JLabel label;
 
-	public UI() {
+	public static int note = 0;
+	public static JLabel LabelLeft;
+	public static String numOfNotes = "amount: 0";
+	String noteString = "notes ";
+	int counter;
+	int typeRight;
+
+	public UI(String s) {
+		super(s);
+
+		JPanel p = new JPanel();
+		label = new JLabel("notes");
+		LabelLeft = new JLabel(numOfNotes);
+
+		p.add(label);
+		p.add(LabelLeft);
+
+		add(p);
+		addKeyListener(this);
+		setSize(200, 100);
+		setVisible(true);
 
 	}
 
 	@Override
-	public void run() {
-		frame = new JFrame("Title");
-		frame.setPreferredSize(new Dimension(200, 100));
-
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-		createComponents(frame.getContentPane());
-
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-	private void createComponents(Container container) {
-
-		GridLayout layout = new GridLayout(1, 3);
-		container.setLayout(layout);
-
-		JTextArea textAreaLeft = new JTextArea(numOfNotes);
-		
-		
-		MessageListener copier = new MessageListener(textAreaLeft, numOfNotes);
-		addButton.addActionListener(copier);
-		playButton.addActionListener(copier);
-
-		container.add(textAreaLeft);
-		container.add(addButton);
-		container.add(playButton);
-		
+	public void keyTyped(KeyEvent e) {
 
 	}
 
-	public JFrame getFrame() {
-		return frame;
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		typeRight = 0;
+
+		if (e.getKeyCode() == KeyEvent.VK_A) {
+			System.out.println("c typed");
+			noteString += "C ";
+			label.setText(noteString);
+			Midi_soundmaker.notes.add(60);
+			typeRight = 1;
+		} else if (e.getKeyCode() == KeyEvent.VK_S) {
+			System.out.println("d typed");
+			noteString += "D ";
+			label.setText(noteString);
+			Midi_soundmaker.notes.add(62);
+			typeRight = 1;
+		} else if (e.getKeyCode() == KeyEvent.VK_D) {
+			System.out.println("e typed");
+			noteString += "E ";
+			label.setText(noteString);
+			Midi_soundmaker.notes.add(64);
+			typeRight = 1;
+		} else if (e.getKeyCode() == KeyEvent.VK_F) {
+			System.out.println("f typed");
+			noteString += "F ";
+			label.setText(noteString);
+			Midi_soundmaker.notes.add(65);
+			typeRight = 1;
+		} else if (e.getKeyCode() == KeyEvent.VK_G) {
+			System.out.println("g typed");
+			noteString += "G ";
+			label.setText(noteString);
+			Midi_soundmaker.notes.add(67);
+			typeRight = 1;
+		} else if (e.getKeyCode() == KeyEvent.VK_H) {
+			System.out.println("a typed");
+			noteString += "A ";
+			label.setText(noteString);
+			Midi_soundmaker.notes.add(69);
+			typeRight = 1;
+		} else if (e.getKeyCode() == KeyEvent.VK_J) {
+			System.out.println("b typed");
+			noteString += "B ";
+			label.setText(noteString);
+			Midi_soundmaker.notes.add(71);
+			typeRight = 1;
+		} else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			Midi_soundmaker player = new Midi_soundmaker();
+			player.setUpPlayer(counter);
+			Midi_soundmaker.notes.clear();
+			counter = 0;
+			UI.LabelLeft.setText("amount: 0");
+
+		}
+
+		if (typeRight == 1) {
+			counter++;
+			numOfNotes = Integer.toString(counter);
+			UI.LabelLeft.setText("amount: " + numOfNotes);
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 	}
 
 }
